3626ff4a45ba8098d28c8263b7a2b1f9d3f21da6
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-04-09 11:46:13 -0700

    Working on TB functionality, WIP
    Co-authored-by: Minerva Enriquez <menriquez-IDM@users.noreply.github.com>

--- tbsim/tb.py
+++ tbsim/tb.py
@@ -9,15 +9,12 @@ __all__ = ['TB']
 class TB(SIR):
     def __init__(self, pars=None, par_dists=None, *args, **kwargs):
         
-        """Add TB parameters and states to the  TB model"""
+        """Add TB parameters and states to the TB model"""
         pars = ss.omergeleft(pars,
             init_prev = 0.01,   # Initial prevalence - TODO: Check if there is one
             beta = 0.5,         # Transmission rate  - TODO: Check if there is one
-            min_age = 0,        # Minimum age for TB infection
-            max_age = 100,      # Maximum age for TB infection
-            seed_infections = [0, 1, 15, 20] # IGNORE, THIS IS ONLY TO debug an issue
         )
-      
+
         """
         DISEASE PROGRESSION: 
         Rates can be interpreted as mean time to transition between states
@@ -26,49 +23,40 @@ class TB(SIR):
         # Natural history according with Stewart slides (EMOD TB model):
         pars = ss.omergeleft(pars,
             # tb_susceptible_to_exposed = 0,        # May be used for susceptibles that migrate to a different zone
-            dur_tb_exposed_to_LF = 0.1,                 # Exposed to Latent Fast 
-            dur_tb_exposed_to_LS = 0.9,                 # Exposed to Latent Slow
-            dur_tb_LF_to_act_pre_sym = 6e-3,            # Latent Fast to Active Pre-Symptomatic     
-            dur_tb_LS_to_act_pre_sym = 3e-5,            # Latent Slow to Active Pre-Symptomatic
+            # TODO: FOR ALL DISTRIBUTIONS...
+            # TODO: Check on lognorm_ex vs lognorm_im in EMOD
+            # TODO: Check on stdev from EMOD
+            p_latent_fast = 0.1, # Probability of latent fast as opposed to latent slow
+            dur_tb_LF_to_act_pre_sym = ss.expon(scale=1/6e-3),  # Latent Fast to Active Pre-Symptomatic     
+            dur_tb_LS_to_act_pre_sym = ss.expon(mean=3e-5),     # Latent Slow to Active Pre-Symptomatic
             
-            dur_tb_act_pre_sym_to_SmP= 0.65,        # Active Presymptomatic to Smear Positive Pulmonary TB    
-            dur_tb_act_pre_sym_to_SmN= 0.25,        # Active Presymptomatic to Smear Negative Pulmonary TB
-            dur_tb_act_pre_sym_to_ExPTB = 0.1,      # Active Presymptomatic to Extra-Pulmonary TB
-                                                # TODO: Check if these values are not contradicting 
-            dur_tb_active_to_LS = 0,                # Smear Positive to Recovered
-            dur_tb_active_to_recovered = 2.4e-4,    # Smear Positive to Recovered
+            p_ExPTB = 0.1,
+            p_smear_positive = 0.65 / (0.65+0.25), # Amongst those without extrapulminary TB
+
+            #dur_tb_active_to_LS = ss.lognorm_ex(mean=0, stdev=0.1),               # Smear Positive to Recovered
+            #dur_tb_active_to_recovered = ss.lognorm_ex(mean=2.4e-4, stdev=0.1),   # Smear Positive to Recovered
             
-            dur_tb_SP_to_dead = 4.5e-4,             # Smear Positive Pulmonary TB to Dead
-            dur_tb_SN_to_dead = 0.3,                # Smear Negative Pulmonary TB to Dead
-            dur_tb_ExPTB_to_dead = 0.15,            # Extra-Pulmonary TB to Dead
+            #dur_tb_SP_to_dead = ss.lognorm_ex(mean=4.5e-4,  stdev=0.1),           # Smear Positive Pulmonary TB to Dead
+            #dur_tb_SN_to_dead = 0.3,                # Smear Negative Pulmonary TB to Dead
+            #dur_tb_ExPTB_to_dead = 0.15,            # Extra-Pulmonary TB to Dead
 
             # Additional parameters:
-            tb_latent_cure = 0.0,               
-            tb_active_cure = 2.4e-4,            
-            tb_presymptomatic_cure_rate = 0.0,  
-            tb_presymptomatic_rate = 3e-2,      
-            tb_inactivation = 0.0,
-            tb_active_period_distribution = ss.random, 
+            #tb_latent_cure = 0.0,               
+            #tb_active_cure = 2.4e-4,            
+            #tb_presymptomatic_cure_rate = 0.0,  
+            #tb_presymptomatic_rate = 3e-2,      
+            #tb_inactivation = 0.0,
+            #tb_active_period_distribution = ss.random, 
         )
         
+        '''
         par_dists = ss.omergeleft(par_dists,
             ## Durations of states
-            dur_tb_exposed_to_LF = ss.lognorm_ex,
-            dur_tb_exposed_to_LS = ss.lognorm_ex,
-            dur_tb_LF_to_act_pre_sym = ss.lognorm_ex,
-            dur_tb_LS_to_act_pre_sym = ss.lognorm_ex,
-
-            dur_tb_act_pre_sym_to_SmP= ss.lognorm_ex,
-            dur_tb_act_pre_sym_to_SmN= ss.lognorm_ex,
-            dur_tb_act_pre_sym_to_ExPTB = ss.lognorm_ex,
-
-            dur_tb_active_to_LS = ss.lognorm_ex,
-            dur_tb_active_to_recovered = ss.lognorm_ex,
-
             dur_tb_SP_to_dead = 4.5e-4,
             dur_tb_SN_to_dead = 0.3,
             dur_tb_ExPTB_to_dead = 0.15,
             )
+        '''
         
         """
         INFECTIOUSNESS:
@@ -85,10 +73,9 @@ class TB(SIR):
             tb_smear_negative_infectiousness =  0.3625,  # [0.25, 0.50],
             tb_extra_pulmonary_infectiousness = .05,  #[0.0, 0.1],
         )
-               
-        
+
         super().__init__(pars=pars, par_dists=par_dists, *args, **kwargs)
-        
+
         self.add_states(
             # Initialize states specific to TB:
             ## Susceptible                              # Existent state part of People
@@ -110,7 +97,14 @@ class TB(SIR):
             ss.State('ti_smear_positive', int, ss.INT_NAN),
             ss.State('ti_smear_negative', int, ss.INT_NAN),
             ss.State('ti_extra_pulmonary', int, ss.INT_NAN),
-            )   
+            )
+
+        # Convert the scalar numbers to a Bernoulli distribution
+        self.pars.p_latent_fast = ss.bernoulli(self.pars.p_latent_fast)
+        self.pars.init_prev = ss.bernoulli(self.pars.init_prev)
+        self.pars.p_ExPTB = ss.bernoulli(self.pars.p_ExPTB)
+        self.pars.p_smear_positive = ss.bernoulli(ss.pars.p_smear_positive)
+
         return
          
     # region Properties
@@ -147,7 +141,7 @@ class TB(SIR):
 
         
         # eligible_uids = ss.true((sim.people.age >= self.pars['init_prev']['age_range'][0]) & (sim.people.age <= self.pars['init_prev']['age_range'][1]))
-        eligible_uids = ss.true((sim.people.age >= self.pars['min_age']) & (sim.people.age <= self.pars['max_age']))
+        # eligible_uids = ss.true((sim.people.age >= self.pars['min_age']) & (sim.people.age <= self.pars['max_age']))
         # initial_cases = self.pars['seed_infections'].filter(eligible_uids)
         self.set_prognoses(sim, eligible_uids)
         # self.set_prognoses(sim, initial_cases)
@@ -179,18 +173,18 @@ class TB(SIR):
         # dur_exp = self.pars['dur_exp'].rvs(uids)
         dur_tb_exposed_to_LF = self.pars['dur_tb_exposed_to_LF'].rvs(uids)
         dur_tb_exposed_to_LS = self.pars['dur_tb_exposed_to_LS'].rvs(uids)
-        dur_presymptomatic = self.pars['dur_active_pre_symptomatic'].rvs(uids)
-        dur_smear_positive = self.pars['dur_smear_positive'].rvs(uids)
-        dur_smear_negative = self.pars['dur_smear_negative'].rvs(uids)
-        dur_extra_pulmonary = self.pars['dur_extra_pulmonary'].rvs(uids)
+
+        # dur_smear_positive = self.pars['dur_smear_positive'].rvs(uids)
+        # dur_smear_negative = self.pars['dur_smear_negative'].rvs(uids)
+        # dur_extra_pulmonary = self.pars['dur_extra_pulmonary'].rvs(uids)
         
         # self.ti_infected[uids] = sim.year + dur_exp
         self.ti_latent_slow[uids] = sim.year + dur_tb_exposed_to_LF
         self.ti_latent_fast[uids] = sim.year + dur_tb_exposed_to_LS
-        self.ti_active_pre_symptomatic[uids] = sim.year + dur_presymptomatic
-        self.ti_smear_positive[uids] = sim.year + dur_smear_positive
-        self.ti_smear_negative[uids] = sim.year + dur_smear_negative
-        self.ti_extra_pulmonary[uids] = sim.year + dur_extra_pulmonary
+
+        # self.ti_smear_positive[uids] = sim.year + dur_smear_positive
+        # self.ti_smear_negative[uids] = sim.year + dur_smear_negative
+        # self.ti_extra_pulmonary[uids] = sim.year + dur_extra_pulmonary
                 
         
         
