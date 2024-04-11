012c23fb6d6f2f0c5f2bdbee8695b6c887ba7a14
Minerva Enriquez <menriquez@idmod.org>
2024-04-11 00:14:21 -0700

    nutrition WIP

--- tbsim/__init__.py
+++ tbsim/__init__.py
@@ -1,1 +1,2 @@
 from .tb import *
+from .nutrition import *
--- tbsim/nutrition.py
+++ tbsim/nutrition.py
@@ -5,27 +5,98 @@ Define non-communicable disease (Nutrition) model
 import numpy as np
 import starsim as ss
 import sciris as sc
+from starsim.diseases.ncd import NCD
 
 __all__ = ['Nutrition']
 
-class Nutrition(ss.Disease):
+class Nutrition(NCD):
     """
     This class implements a basic Nutrition model with risk of developing a condition
     (e.g., hypertension, diabetes), a state for having a condition, or accelerate a condition 
     (e.g., TB, HIV) and associated mortality.
     """
-    def __init__(self, params=None, param_dists=None, param_defaults=None):
+    # Possible references:
+    # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10876842/
+    # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9971264/
+    # https://www.espen.org/files/ESPEN-guidelines-on-definitions-and-terminology-of-clinical-nutrition.pdf 
+    
+    def __init__(self, pars=None):
         # According to https://www.who.int/news-room/questions-and-answers/item/malnutrition
+        # Actual Disease class initialization.
+        super().__init__(pars=pars)
 
         # Generic Nutrition States
         self.add_states(
-            ss.State('normal', bool, True),                     # Normal nutrition
+            # ss.State('at_risk', bool, True),                     # At risk for malnutrition - PART OF NCD BASE CLASS
+            ss.State('normal', bool, True),                     # Normal nutrition state (no malnutrition or overnutrition
             ss.State('undernutrition', bool, False),            # malnutrition, this includes stunting, wasting, underweight 
             ss.State('overnutrition', bool, False),             # overweight, obesity, and the extention of this would be the related NCD  
             ss.State('micronutrient_deficiency', bool, False),  # micronutrient-imbalance:  allow use of the disease state without being too specific
         )
+        self.add_states(
+            ss.State('ti_undernutrition', int, ss.INT_NAN),     # Time at which undernutrition was diagnosed
+            ss.State('ti_overnutrition', int, ss.INT_NAN),      # Time at which overnutrition was diagnosed
+            ss.State('ti_micronutrient_deficiency', int, ss.INT_NAN), # Time at which micronutrient deficiency was diagnosed
+        )
+        return
+
+    @property
+    def not_at_risk(self):
+        return ~self.at_risk
+
+    def update_pre(self, sim):
+        # Make all the updates from the NCD model 
+        super().update_pre(sim)
         
-        # Subforms of malnutrition Nutrition States
+        # At risk --> Undernutrition
+        inds = ss.true(self.normal & (self.ti_undernutrition <= sim.ti))
+        if len(inds):
+            self.normal[inds] = False
+            self.undernutrition[inds] = True
+            
+        inds = ss.true(self.normal & (self.ti_overnutrition <= sim.ti))
+        if len(inds):
+            self.normal[inds] = False
+            self.undernutrition[inds] = True
+            
+        inds = ss.true(self.normal & (self.ti_micronutrient_deficiency <= sim.ti))
+        if len(inds):
+            self.normal[inds] = False
+            self.undernutrition[inds] = True
+            
+        return
+
+    def make_new_cases(self, sim):
+        super().make_new_cases(sim)
+
+        new_cases = ss.true(self.ti_undernutrition == sim.ti)
+        self.undernutrition[new_cases] = True
+        # prog_years = self.pars.prognosis.rvs(new_cases)
+        # self.ti_dead[new_cases] = sim.ti + sc.randround(prog_years / sim.dt)
+
+        self.set_prognoses(sim, new_cases)
+        return new_cases
+    
+    def set_prognoses(self, sim, new_cases):
+        super().set_prognoses(sim, new_cases)
+        return
+
+
+
+class MalnutritionX(NCD):
+    """_summary_
+    This class implements a basic Malnutrition model with risk of developing a condition
+    including subforms of malnutrition.
+    """
+    def __init__(self, pars=None, par_dists=None, *args, **kwargs):
+
+
+        pars = ss.omergeleft(pars,
+            init_prev = 0.01,   # Initial prevalence - TODO: Check if there is one
+            beta = 0.25,         # Transmission rate  - TODO: Check if there is one
+        )
+        
+                # Subforms of malnutrition Nutrition States
         self.add_states(
             ss.State('stunting', bool, False),                  # undernutrition sub-form: low height for age
             ss.State('wasting', bool, False),                   # undernutrition sub-form: low weight for height
@@ -37,75 +108,3 @@ class Nutrition(ss.Disease):
             ss.State('overweight', bool, False),                # BMI of 30 or more
             ss.State('obesity', bool, False),                   # BMI of 30 or more
         )
-        
-        params = ss.omergeleft(params, 
-            # TODO: Natural history parameters, specify them in <days|weeks|months|years> 
-            # TODO: Decide if makes sense to have these values incorporated and if it's possible to get them
-            # one possibility that I found was: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10876842/
-                dur_undernutrition = 0,            # Add source
-                dur_overnutrition= 0,              # Add source
-                dur_micronutrient_deficiency = 0,  # Add source
-                )
-        
-        param_dists = ss.omergeleft(param_dists, 
-            # TODO: Does it makes sense to only have the 3 mayor subdivitions of malnutrtion for param_dists duration?
-            dur_undernutrition = ss.lognorm_ex,                 
-            dur_overnutrition = ss.lognorm_ex,                      
-            dur_micronutrient_imbalance = ss.lognorm_ex,
-            )
-        
-        param_defaults = dict(
-            prognosis = ss.weibull_min(c=2, scale=5),   # Time in years between first becoming undernutrition and death
-            initial_risk = .07                          # Baseline case 
-        )
-
-        # Actual Disease class initialization.
-        super().__init__(ss.omerge( params, param_dists, param_defaults))
-        return
-
-    @property
-    def not_normal(self):
-        return ~self.normal
-
-    def set_initial_states(self, sim):
-        """
-        Setting initial nutrition states
-        """
-        alive_uids = ss.true(sim.people.alive)      
-        initial_risk = self.params['initial_risk'].filter(alive_uids)
-        # TODO: in-progress module.
-        self.normal[initial_risk] = True
-        self.ti_undernutrition[initial_risk] = sim.ti + sc.randround(self.params['dur_risk'].rvs(initial_risk) / sim.dt)
-        return initial_risk
-
-    def make_new_cases(self, sim):
-        # TODO: in-progress module.
-        new_cases = ss.true(self.ti_undernutrition == sim.ti)
-        self.undernutrition[new_cases] = True
-        prog_years = self.params['prognosis'].rvs(new_cases)
-        self.ti_dead[new_cases] = sim.ti + sc.randround(prog_years / sim.dt)
-        super().set_prognoses(sim, new_cases)
-        return new_cases
-
-    # def init_results(self, sim):
-    #     """
-    #     Initialize results
-    #     """
-    #     super().init_results(sim)
-    #     self.results += [
-    #         ss.Result(self.name, 'n_not_normal', sim.npts, dtype=int),
-    #         ss.Result(self.name, 'prevalence', sim.npts, dtype=float),
-    #         ss.Result(self.name, 'new_deaths', sim.npts, dtype=int),
-    #     ]
-    #     return
-
-    # def update_results(self, sim):
-    #     super().update_results(sim)
-    #     ti = sim.ti
-    #     alive = sim.people.alive
-    #     self.results.n_not_normal[ti] = np.count_nonzero(self.not_normal & alive)
-    #     self.results.prevalence[ti]    = np.count_nonzero(self.undernutrition & alive)/alive.count()
-    #     self.results.new_deaths[ti]    = np.count_nonzero(self.ti_dead == ti)
-    #     return
-
-
