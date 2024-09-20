1a3b0a3f95d469aa82d14904a6a742ce720db36d
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-09-20 09:58:00 -0700

    Small bump to version.

--- tbsim/version.py
+++ tbsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.3.0'
-__versiondate__ = '2024-09-04'
+__version__ = '0.3.1'
+__versiondate__ = '2024-09-20'
 __license__ = f'tbsim {__version__} ({__versiondate__}) — © 2023-2024 by IDM'
