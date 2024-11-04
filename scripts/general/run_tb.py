7e295c32e7d0b996480bdf447a67ff1dfd8237ba
vgdeven <vgdeven@gmail.com>
2024-11-04 11:04:24 -0800

    updated the run_tb example

--- scripts/general/run_tb.py
+++ scripts/general/run_tb.py
@@ -1,61 +1,26 @@
 import tbsim as mtb
 import starsim as ss
+import sciris as sc
 import matplotlib.pyplot as plt
 
-
-def make_tb():
-    # --------------- People ----------
-    n_agents = 1000
-    pop = ss.People(n_agents=n_agents)
-
-    # --------------- TB disease --------
-    tb_pars = dict(  # Disease parameters
-        beta = 0.001, 
-        init_prev = 0.25,
-        )
-    tb = mtb.TB(tb_pars) # Initialize
-
-    # --------------- Network ---------
-    net_pars = dict(    # Network parameters
-        n_contacts=ss.poisson(lam=5),
-        dur = 0, # End after one timestep
-        )
-    net = ss.RandomNet(net_pars)  # Initialize a random network
-
-    # --------------- Demographics --------
-    dems = [
-        ss.Pregnancy(pars=dict(fertility_rate=15)), # Per 1,000 people
-        ss.Deaths(pars=dict(death_rate=10)), # Per 1,000 people
-    ]
-
-    # --------------- simulation -------
-    sim_pars = dict(    # define simulation parameters
-        dt = 7/365,
-        start = 1990,
-        stop = 2010,            # Stop after 20 years
-        )
-    sim = ss.Sim(people=pop, networks=net, diseases=tb, pars=sim_pars, demographics=dems)   # initialize the simulation
-    sim.pars.verbose = sim.pars.dt / 5      # Print status every 5 years instead of every 10 steps
-    return sim
-
-def make_tb_simplified(agents=1000, start=2000, dt=7/365):
-    pop = ss.People(n_agents=agents)
+def make_tb_simplified(sim_pars = dict(unit = 'day', dt=7, 
+                                       start=sc.date('2013-01-01'), 
+                                       stop=sc.date('2016-12-31'), 
+                                       rand_seed=123)):
+    
+    pop = ss.People(n_agents=1000)
     tb = mtb.TB(dict(beta = 0.001, init_prev = 0.25))
     net = ss.RandomNet(dict(n_contacts=ss.poisson(lam=5), dur = 0))
     dems = [ss.Pregnancy(pars=dict(fertility_rate=15)), ss.Deaths(pars=dict(death_rate=15))]
-    sim = ss.Sim(people=pop, networks=net, diseases=tb, pars=dict(dt = dt, start = start, dur=10), demographics=dems)   # Using duration instead of stop.
-    sim.pars.verbose = sim.pars.dt / 5
+    sim = ss.Sim(people=pop, networks=net, diseases=tb, demographics=dems, pars=sim_pars)   # Using duration instead of stop.
+    
+    sim.pars.verbose = sim.pars.dt / 365
+    
     return sim
 
 if __name__ == '__main__':
    
-    sim_tb = make_tb()
-    sim_tb.run()
-    sim_tb.diseases['tb'].plot()
-    # mtb.plot_sim(sim_tb)
-    plt.show()
-
-    sim_tb = make_tb_simplified(agents=1500, start=2000, dt=7/365)
+    sim_tb = make_tb_simplified()
     sim_tb.run()
     sim_tb.diseases['tb'].plot()
     # mtb.plot_sim(sim_tb)
