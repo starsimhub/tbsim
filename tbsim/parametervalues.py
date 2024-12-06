b4479aad257de48f92b403a6c99df4df067880ea
Minerva Enriquez <menriquez@idmod.org>
2024-12-05 17:52:06 -0800

    wip

--- tbsim/parametervalues.py
+++ tbsim/parametervalues.py
@@ -1,63 +1,6 @@
 import starsim as ss
 import numpy as np
 
-class RatesByAge:
-
-    def __init__(self, unit, dt):
-        self.unit = unit
-        self.dt = dt
-        self.rates_dict = {
-            'rate_LS_to_presym': {
-                0: ss.perday(3e-5, unit, dt),   
-                15: ss.perday(2.0548e-6, unit, dt),  
-                25: ss.perday(3e-5, unit, dt),
-                np.inf: ss.perday(3e-5, unit, dt),
-            },
-            'rate_LF_to_presym': {
-                0: ss.perday(6e-3, unit, dt),
-                15: ss.perday(4.5e-3, unit, dt),
-                25: ss.perday(6e-3, unit, dt),
-                np.inf: ss.perday(6e-3, unit, dt),
-            },
-            'rate_presym_to_active': {
-                0: ss.perday(3e-2, unit, dt),
-                15: ss.perday(5.48e-3, unit, dt),
-                25: ss.perday(3e-2, unit, dt),
-                np.inf: ss.perday(6e-3, unit, dt),
-            },
-            'rate_active_to_clear': {
-                0: ss.perday(2.4e-4, unit, dt),
-                15: ss.perday(2.74e-4, unit, dt),
-                25: ss.perday(2.4e-4, unit, dt),
-                np.inf: ss.perday(2.4e-4, unit, dt),
-            },
-            'rate_smpos_to_dead': {
-                0: ss.perday(4.5e-4, unit, dt),
-                15: ss.perday(6.85e-4, unit, dt),
-                25: ss.perday(4.5e-4, unit, dt),
-                np.inf: ss.perday(4.5e-4, unit, dt),
-            },
-            'rate_smneg_to_dead': {
-                0: ss.perday(0.3 * 4.5e-4, unit, dt),
-                15: ss.perday(2.74e-4, unit, dt),
-                25: ss.perday(0.3 * 4.5e-4, unit, dt),
-                np.inf: ss.perday(0.3 * 4.5e-4, unit, dt),
-            },
-            'rate_exptb_to_dead': {
-                0: ss.perday(0.15 * 4.5e-4, unit, dt),
-                15: ss.perday(2.74e-4, unit, dt),
-                25: ss.perday(0.15 * 4.5e-4, unit, dt),
-                np.inf: ss.perday(0.15 * 4.5e-4, unit, dt),
-            },
-            'rate_treatment_to_clear': {
-                0: ss.peryear(12/2, unit, dt),
-                15: ss.peryear(2, unit, dt),
-                25: ss.peryear(12/2, unit, dt),
-                np.inf: ss.perday(12/2, unit, dt),
-            },
-        }
-        return 
- 
 
 class RateVec:
     def __init__(self, cutoffs, values, interpolation="stair"):
@@ -113,3 +56,60 @@ class RateVec:
     def __summary__(self):
         return f"RateVec(cutoffs={self.cutoffs}, values={self.values}, interpolation={self.interpolation})"
 
+
+class RatesByAge:
+    def __init__(self, unit, dt):
+        self.unit = unit
+        self.dt = dt
+        self.rates_dict = {
+            'rate_LS_to_presym': {
+                0: ss.perday(3e-5, unit, dt),   
+                15: ss.perday(2.0548e-6, unit, dt),  
+                25: ss.perday(3e-5, unit, dt),
+                np.inf: ss.perday(3e-5, unit, dt),
+            },
+            'rate_LF_to_presym': {
+                0: ss.perday(6e-3, unit, dt),
+                15: ss.perday(4.5e-3, unit, dt),
+                25: ss.perday(6e-3, unit, dt),
+                np.inf: ss.perday(6e-3, unit, dt),
+            },
+            'rate_presym_to_active': {
+                0: ss.perday(3e-2, unit, dt),
+                15: ss.perday(5.48e-3, unit, dt),
+                25: ss.perday(3e-2, unit, dt),
+                np.inf: ss.perday(6e-3, unit, dt),
+            },
+            'rate_active_to_clear': {
+                0: ss.perday(2.4e-4, unit, dt),
+                15: ss.perday(2.74e-4, unit, dt),
+                25: ss.perday(2.4e-4, unit, dt),
+                np.inf: ss.perday(2.4e-4, unit, dt),
+            },
+            'rate_smpos_to_dead': {
+                0: ss.perday(4.5e-4, unit, dt),
+                15: ss.perday(6.85e-4, unit, dt),
+                25: ss.perday(4.5e-4, unit, dt),
+                np.inf: ss.perday(4.5e-4, unit, dt),
+            },
+            'rate_smneg_to_dead': {
+                0: ss.perday(0.3 * 4.5e-4, unit, dt),
+                15: ss.perday(2.74e-4, unit, dt),
+                25: ss.perday(0.3 * 4.5e-4, unit, dt),
+                np.inf: ss.perday(0.3 * 4.5e-4, unit, dt),
+            },
+            'rate_exptb_to_dead': {
+                0: ss.perday(0.15 * 4.5e-4, unit, dt),
+                15: ss.perday(2.74e-4, unit, dt),
+                25: ss.perday(0.15 * 4.5e-4, unit, dt),
+                np.inf: ss.perday(0.15 * 4.5e-4, unit, dt),
+            },
+            'rate_treatment_to_clear': {
+                0: ss.peryear(12/2, unit, dt),
+                15: ss.peryear(2, unit, dt),
+                25: ss.peryear(12/2, unit, dt),
+                np.inf: ss.perday(12/2, unit, dt),
+            },
+        }
+        return 
+ 
