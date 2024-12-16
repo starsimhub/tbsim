38181a7158cacb7ab0600ee8d33c2b4fb22e1ca8
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-12-16 00:16:30 -0800

    Works

--- scripts/ACT3/run_act3.py
+++ scripts/ACT3/run_act3.py
@@ -38,7 +38,7 @@ def run_ACF(base_sim, skey, scen, rand_seed=0):
     acf_res = []
     for s in ms.sims:
         df = pd.DataFrame({
-            'timevec': s.results.timevec,
+            'time_year': s.results.timevec,
             'on_treatment': s.results.tb.n_on_treatment, 
             'prevalence': s.results.tb.prevalence,
             'active_presymp': s.results.tb.n_active_presymp,
@@ -47,20 +47,20 @@ def run_ACF(base_sim, skey, scen, rand_seed=0):
         })
         df['scenario'] = skey
         df['arm'] = s.label
-        df['seed'] = s.pars.rand_seed
+        df['rand_seed'] = s.pars.rand_seed
         tb_res.append(df)
 
         act3_dates = [ss.date(t) for t in ['2014-06-01', '2015-06-01', '2016-06-01', '2017-06-01']]
         inds = np.searchsorted(s.results.timevec, act3_dates, side='left')
         df = pd.DataFrame({
-            'timevec': s.results.timevec[inds],
+            'time_year': s.results.timevec[inds],
             'n_elig': s.results['ACT3 Active Case Finding'].n_elig[inds],
             'n_tested': s.results['ACT3 Active Case Finding'].n_tested[inds],
             'n_positive': s.results['ACT3 Active Case Finding'].n_positive[inds],
         })
         df['scenario'] = skey
         df['arm'] = s.label
-        df['seed'] = s.pars.rand_seed
+        df['rand_seed'] = s.pars.rand_seed
         acf_res.append(df)
 
     tb_res = pd.concat(tb_res)
@@ -128,8 +128,8 @@ if __name__ == '__main__':
 
     # MOVE TO aplt:
     import seaborn as sns
-    ret = df_result.get('TB').reset_index(drop=True).melt(id_vars=['scenario', 'timevec', 'arm', 'seed'], value_name='value', var_name='variable')
-    g = sns.relplot(data=ret, x='timevec', y='value', hue='arm', col='variable', kind='line', row='scenario', errorbar='sd', facet_kws={'sharey': False}, height=3, aspect=1.4) # SD for speed, units='seed'
+    ret = df_result.get('TB').reset_index(drop=True).melt(id_vars=['scenario', 'time_year', 'arm', 'rand_seed'], value_name='value', var_name='variable')
+    g = sns.relplot(data=ret, x='time_year', y='value', hue='arm', col='variable', kind='line', row='scenario', errorbar='sd', facet_kws={'sharey': False}, height=3, aspect=1.4) # SD for speed, units='rand_seed'
     g.set_titles(col_template="{col_name}")
     g.fig.tight_layout()
     g.fig.savefig(os.path.join(resdir, 'figs', 'timeseries.png'), dpi=600)
