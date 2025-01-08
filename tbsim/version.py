93e313b15f1d2c6a6159617480b68298435208b0
Daniel Klein <daniel.klein@gatesfoundation.org>
2025-01-07 16:44:24 -0800

    Removing ACT3 now that we have the dedicated tb_acf repo

--- tbsim/version.py
+++ tbsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.3.1'
-__versiondate__ = '2024-09-20'
-__license__ = f'tbsim {__version__} ({__versiondate__}) — © 2023-2024 by IDM'
+__version__ = '0.4.0'
+__versiondate__ = '2025-01-07'
+__license__ = f'tbsim {__version__} ({__versiondate__}) — © 2023-2025 by IDM'
