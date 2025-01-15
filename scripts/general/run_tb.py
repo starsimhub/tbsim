4d225c484f6af4955d71826cf1f9db3f5ff25ddf
Minerva Enriquez <74386190+menriquez-IDM@users.noreply.github.com>
2025-01-14 16:03:08 -0800

    adding more plot and diagram samples

--- scripts/general/run_tb.py
+++ scripts/general/run_tb.py
@@ -14,15 +14,15 @@ def make_tb(sim_pars=None):
     if sim_pars is not None:
         spars.update(sim_pars)
 
-    pop = ss.People(n_agents=500)
+    pop = ss.People(n_agents=100)
     tb = mtb.TB(dict(
         beta = ss.beta(0.1),
         init_prev = ss.bernoulli(p=0.25),
         unit = 'day'
     ), validate_dwell_times=True)
     net = ss.RandomNet(dict(n_contacts=ss.poisson(lam=5), dur=0))
-    births = ss.Births(pars=dict(birth_rate=15))
-    deaths = ss.Deaths(pars=dict(death_rate=15))
+    births = ss.Births(pars=dict(birth_rate=5))
+    deaths = ss.Deaths(pars=dict(death_rate=5))
 
     sim = ss.Sim(
         people=pop,
@@ -40,14 +40,15 @@ if __name__ == '__main__':
     sim_tb.run()
     tb = sim_tb.diseases['tb']
 
-    # tb.validate_dwell_time_distributions() # This is an optional step
+    tb.validate_dwell_time_distributions() # This is an optional step
     # These are two of the few plots that have been added directly to the DwellTimeAnalyzer class
-    # tb.plot_dwell_time_validation()        
-    # tb.dwell_time_analyzer.plot_dwell_time_validation_interactive()
+    tb.plot_dwell_time_validation()        
+    tb.dwell_time_analyzer.plot_dwell_time_validation_interactive()
 
     # Otherwise, use the function to save the dwell time distributions to a file and then plot them
 
     file_dwt = tb.dwell_time_analyzer.save_to_file()
+    mtb.sankey(file_dwt)
     mtb.stacked_bars_states_per_agent_clean(file_dwt)
     mtb.plot_dwell_time_lines_for_each_agent(file_dwt)
     # plt.show()
