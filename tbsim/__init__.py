3fc049cacfa13d105ab2d848b1faebb83fbfe8bf
Minerva Enriquez <74386190+menriquez-IDM@users.noreply.github.com>
2025-05-18 11:46:09 -0700

    refresh from main (#129)
    * hiv light class - initial draft
    
    * Adapting connector to updated HIV states
    
    * Adding on art.
    
    * Adding onArt and calling hiv stage 'ACUTE' instead
    
    * fixing progression of states
    
    * vectorizing connector functions. Increasing sim duration.
    
    * refactoring connectors classes
    
    * updates to the connector to use a more performant approach.
    
    * adding prevalence and on_ART functions to equilibrate their values
    
    * in progress - adding funtions to equilibrate infection and art
    
    * wip - adding interventions and v1 of scripts using the connector
    
    * fixing the handling of art when no intervention is used (for baseline)
    
    * Adding start, stop.
    
    * tb+hiv+interventions  scenarios
    
    * adding baseline case and cleaning up the code.
    
    * model parameters update - changing upper case for lower case
    
    * adding Hiv Intervention mode class to facilitate remembering the available options.
    
    * adding hiv comorbidity notes
    
    * adding tutorials and examples
    
    * adding tutorial for tb hiv scenarios
    
    * updating some parameters to run_tbhiv_scens notebook
    
    * Adding a cleaner sample
    
    * Possible bug.
    
    * Setting init_prev and init_onart to zeros to make sure they are set with the values requested by the user.
    
    * updates to test scripts
    
    * making connector multipliers part of the test scripts.
    
    * adding deve's scenarios; fixing intervention issues and updating the hiv states.
    
    * fixing notebook parameters.
    
    * fixing the scenario's rate.
    
    * updating sample max value function and adding sample common functions
    
    * making the hiv intervention function name and parameters more descriptive
    
    * chanfing hiv paraneter name,

--- tbsim/__init__.py
+++ tbsim/__init__.py
@@ -2,12 +2,16 @@ from os.path import dirname, join as joinpath
 DATADIR = joinpath(dirname(__file__), 'data')
 
 from .tb import TB, TBS
-from .malnutrition import Malnutrition
+from .comorbidities.malnutrition.malnutrition import Malnutrition
+from .comorbidities.hiv.hiv import HIV
+from .comorbidities.hiv.hiv import HIVState
+from .comorbidities.hiv.intervention import HivInterventions
 from .config import *
 from .interventions.interventions import *
 from .interventions.tpt import *
 from .interventions.bcg import *
 from .interventions.cascadecare import *
 from .networks import HouseholdNet
-from .connectors import TB_Nutrition_Connector
+from .comorbidities.hiv.tb_hiv_cnn import *
+from .comorbidities.malnutrition.tb_malnut_cnn import TB_Nutrition_Connector
 from .analyzers import DwtAnalyzer, DwtPlotter, DwtPostProcessor
