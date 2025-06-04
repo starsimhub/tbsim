ccb199c5750ba20781bbd1410726f50b9981806f
Minerva Enriquez <74386190+menriquez-IDM@users.noreply.github.com>
2025-06-04 13:32:42 -0700

    moving Agents wrapper into a separate file

--- tbsim/__init__.py
+++ tbsim/__init__.py
@@ -15,4 +15,5 @@ from .networks import HouseholdNet
 from .comorbidities.hiv.tb_hiv_cnn import *
 from .comorbidities.malnutrition.tb_malnut_cnn import TB_Nutrition_Connector
 from .analyzers import DwtAnalyzer, DwtPlotter, DwtPostProcessor
-from .utils import Agents
+from .utils import *
+from .wrappers import Agents
