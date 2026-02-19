58241d7899da1a8a27eaabe45f03138f0ba62461
Cliff Kerr <questionably@gmail.com>
2026-02-19 17:04:33 -0500

    update imports

--- tbsim/__init__.py
+++ tbsim/__init__.py
@@ -1,26 +1,16 @@
-from os.path import dirname, join as joinpath
-DATADIR = joinpath(dirname(__file__), 'data')
+import os
+DATADIR = os.path.join(os.path.dirname(__file__), 'data')
 
-
-from .tb_lshtm import TB_LSHTM, TB_LSHTM_Acute, TBSL
-from .tb import TB, TBS
-from .comorbidities.malnutrition.malnutrition import Malnutrition
-from .comorbidities.hiv.hiv import HIV
-from .comorbidities.hiv.hiv import HIVState
-from .comorbidities.hiv.intervention import HivInterventions
-from .utils.config import *
-from .interventions.interventions import *
-from .interventions.healthseeking import *
-from .interventions.tb_diagnostic import *
-from .interventions.tb_treatment import *
-from .interventions.tpt import *
-from .interventions.bcg import *
-from .interventions.beta import BetaByYear
-from .interventions.tb_drug_types import *
-from .interventions.tb_drug_types import get_dots_parameters, get_drug_parameters, get_all_drug_parameters
-from .networks import HouseholdNet
+from .tb_lshtm import *
+from .tb import *
+from .comorbidities.malnutrition.malnutrition import *
+from .comorbidities.hiv.hiv import *
+from .comorbidities.hiv.intervention import *
 from .comorbidities.hiv.tb_hiv_cnn import *
-from .comorbidities.malnutrition.tb_malnut_cnn import TB_Nutrition_Connector
-from .analyzers import DwtAnalyzer, DwtPlotter, DwtPostProcessor
+from .comorbidities.malnutrition.tb_malnut_cnn import *
+from .utils.config import *
+from .interventions import *
+from .networks import *
+from .analyzers import *
 from .utils.plots import *
 from .people import TBPeople
