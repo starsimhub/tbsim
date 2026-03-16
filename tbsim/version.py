405dc3e483a7fcd80916f856ddfc5ab1a37edd2f
Cliff Kerr <questionably@gmail.com>
2026-03-16 12:21:26 -0400

    update changelog

--- tbsim/version.py
+++ tbsim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.7.0'
-__versiondate__ = '2026-03-12'
+__versiondate__ = '2026-03-16'
 __license__ = f'TBsim {__version__} ({__versiondate__}) — © 2023-2026 by IDM'
